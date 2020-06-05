f116974dafb418799fa02f5ef0c2c42a34bedb47
broana-ait <torra.agustinignacio@gmail.com>
2020-06-04 23:47:53 -0300

    update app component to show a simple toast notification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
+toast.configure();
 function App() {
-  return (
+	const notify = () => {
+		toast("Basic notification!");
+	};
+
+	return (
     <div className="App">
-      <header className="App-header">
+      <button onClick={notify}>Notify!</button>
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +26,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+		</div>
+	);
 }
 
 export default App;
